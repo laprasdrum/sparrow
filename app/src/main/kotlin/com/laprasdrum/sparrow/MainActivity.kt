339f806efd7681f7aa9548f05dfb5e58bffca743
Yuya Moriguchi <laprasdrum@c-fo.com>
2015-07-15 21:22:23 +0900

    add tablayout & viewpager

--- app/src/main/kotlin/com/laprasdrum/sparrow/MainActivity.kt
+++ app/src/main/kotlin/com/laprasdrum/sparrow/MainActivity.kt
@@ -1,77 +1,63 @@
 package com.laprasdrum.sparrow
 
 import android.os.Bundle
+import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
-import android.view.*
-import java.util.Locale
+import android.support.v7.widget.Toolbar
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import kotlin.properties.Delegates
 
 public class MainActivity : AppCompatActivity() {
 
-    var mSectionsPagerAdapter: SectionsPagerAdapter by Delegates.notNull()
-
-    var mViewPager: ViewPager by Delegates.notNull()
+    var pagerAdapter: SectionsPagerAdapter by Delegates.notNull()
+    var viewPager: ViewPager by Delegates.notNull()
+    var tabLayout: TabLayout by Delegates.notNull()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var toolbar: Toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
 
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = SectionsPagerAdapter(getSupportFragmentManager())
+        pagerAdapter = SectionsPagerAdapter(getSupportFragmentManager())
 
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = findViewById(R.id.pager) as ViewPager
-        mViewPager.setAdapter(mSectionsPagerAdapter)
+        viewPager = findViewById(R.id.pager) as ViewPager
+        viewPager.setAdapter(pagerAdapter)
 
+        viewPager.addOnPageChangeListener(object : ViewPager.SimpleOnPageChangeListener() {
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+            }
+
+            override fun onPageSelected(position: Int) {
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+            }
+        })
+
+        tabLayout = findViewById(R.id.tab_layout) as TabLayout
+        tabLayout.setupWithViewPager(viewPager)
     }
 
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        val id = item!!.getItemId()
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true
-        }
-
-        return super.onOptionsItemSelected(item)
-    }
-
-
-    /**
-     * A [FragmentPagerAdapter] that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
     public inner class SectionsPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
         override fun getItem(position: Int): Fragment {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
             return PlaceholderFragment.newInstance(position + 1)
         }
 
         override fun getCount(): Int {
-            // Show 3 total pages.
             return 3
         }
 
         override fun getPageTitle(position: Int): CharSequence? {
-            val l = Locale.getDefault()
             when (position) {
                 0 -> return getString(R.string.title_section1)
                 1 -> return getString(R.string.title_section2)
@@ -81,27 +67,28 @@ public class MainActivity : AppCompatActivity() {
         }
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
     public class PlaceholderFragment : Fragment() {
 
         override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
             val rootView = inflater!!.inflate(R.layout.fragment_main, container, false)
+            val labelView: TextView = rootView.findViewById(R.id.label) as TextView
+
+            val position: Int = getArguments().getInt(ARG_SECTION_NUMBER)
+            val label = when (position) {
+                1 -> "page 1"
+                2 -> "page 2"
+                3 -> "page 3"
+                else -> {
+                    "no position matched"
+                }
+            }
+            labelView.setText(label)
             return rootView
         }
 
         companion object {
-            /**
-             * The fragment argument representing the section number for this
-             * fragment.
-             */
             private val ARG_SECTION_NUMBER = "section_number"
 
-            /**
-             * Returns a new instance of this fragment for the given section
-             * number.
-             */
             public fun newInstance(sectionNumber: Int): PlaceholderFragment {
                 val fragment = PlaceholderFragment()
                 val args = Bundle()
